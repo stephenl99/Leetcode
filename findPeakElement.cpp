bf7a8e732af4dead8daf267ad84402b8da93a848
slinder8 <sjtlinder@icloud.com>
2025-08-31 18:00:22 -0400

    find peak

---
+++ findPeakElement.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (mid == 0) {
+                if (nums[mid] > nums[mid + 1]) {
+                    return mid;
+                } else {
+                    return mid + 1;
+                }
+            }
+            if (mid == nums.size() - 1) {
+                if (nums[mid] > nums[mid - 1]) {
+                    return mid;
+                } else {
+                    return mid - 1;
+                }
+            }
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] < nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+                right = mid - 1;
+            } else if (nums[mid] < nums[mid - 1]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
