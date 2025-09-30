a1d58080602ddfe7e1a809f5b48cc8b67e2cb9f5
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2025-09-30 17:15:47 -0400

    Add LeetCode solution: 10. Regular Expression Matching

--- 10-regular-expression-matching.cpp
+++ 10-regular-expression-matching.cpp
@@ -1,15 +1,24 @@
-class Solution {
-public:
-    bool isMatch(string s, string p) {
-        if (p.size() == 0 && s.size() == 0) {
-            return true;
-        } else if (p.size() == 0) {
-            return false;
-        }
-        bool match = (s.size() > 0 && (p.at(0) == s.
-at(0) || p.at(0) == '.'));
-        if (p.size() >= 2 && p.at(1) == '*') {
-            if (isMatch(s, p.substr(2))) {
-                return true;
             }
             if (match && isMatch(s.substr(1), p)) {
+                return true;
+            if (isMatch(s, p.substr(2))) {
+        if (p.size() >= 2 && p.at(1) == '*') {
+at(0) || p.at(0) == '.'));
+        bool match = (s.size() > 0 && (p.at(0) == s.
+        }
+            return false;
+        } else if (p.size() == 0) {
+            return true;
+        if (p.size() == 0 && s.size() == 0) {
+    bool isMatch(string s, string p) {
+public:
+class Solution {
+                return true;
+            }
+        } else {
+            return match && isMatch(s.substr(1), p.
+substr(1));
+        }
+        return false;
+    }
+};
