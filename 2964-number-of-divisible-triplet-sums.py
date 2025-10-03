e6d9673a18146c03b0b11cb5ce3cb8e6ab11dd86
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2025-10-02 20:02:53 -0400

    Add LeetCode solution: 2964. Number Of Divisible Triplet Sums

--- 2964-number-of-divisible-triplet-sums.py
+++ 2964-number-of-divisible-triplet-sums.py
@@ -1,13 +1,9 @@
 class Solution:
     def divisibleTripletCount(self, nums: List[int], d: int) -> int:
+        curr = defaultdict(int)
         count = 0
-        remainder = defaultdict(int)
-        for j in range(len(nums)-2, 0, -1):
-            k = j+1
-            remainder[(d-nums[k]) % d] += 1
+        for j in range(len(nums) - 2, -1, -1):
+            curr[nums[j+1] % d] += 1
             for i in range(j):
-                count += remainder[(nums[i] + nums[j]) % d]
+                count += curr[(d - (nums[i] + nums[j]) % d) % d]
         return count
-
-
-
