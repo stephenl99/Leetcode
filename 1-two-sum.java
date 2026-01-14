bbdc99df3ce262a509ac062fd457cf02fd0c4af3
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2026-01-13 21:27:12 -0500

    Add LeetCode solution: 1. Two Sum

--- 1-two-sum.java
+++ 1-two-sum.java
@@ -1,16 +1,16 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
-        HashMap<Integer, Integer> set = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (set.containsKey(target - nums[i])) {
-                answer[0] = set.get(target - nums[i]);
-                answer[1] = i;
-                return answer;
-            } else {
-                set.put(nums[i], i);
-            }
-        }
-        return null;
-    }
-}
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int[] answer = new int[2];
+4        HashMap<Integer, Integer> set = new HashMap<>();
+5        for (int i = 0; i < nums.length; i++) {
+6            if (set.containsKey(target - nums[i])) {
+7                answer[0] = set.get(target - nums[i]);
+8                answer[1] = i;
+9                return answer;
+10            } else {
+11                set.put(nums[i], i);
+12            }
+13        }
+14        return null;
+15    }
+16}
