7d59ef123ab8c40d1023ce77219b086adb292fe4
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2026-08-25 16:05:36 -0400

    Add LeetCode solution: 2257. Count Unguarded Cells In The Grid

--- 2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid.cpp
@@ -1,46 +1,65 @@
-struct VectorHash {
-    size_t operator()(const vector<int>& v) const {
-        size_t h = 0;
-        for (int x : v)
-            h ^= hash<int>{}(x) + 0x9e3779b9 + (h << 6) + (h >> 2);
-        return h;
-    }
-};
-
 class Solution {
 public:
-    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        unordered_set<vector<int>, VectorHash> wallSet(walls.begin(), walls.end());
-        unordered_set<vector<int>, VectorHash> guardSet(guards.begin(), guards.end());
+    int countUnguarded(int m, int n, vector<vector<int>>& guards,
+                       vector<vector<int>>& walls) {
+
+        unordered_set<int> wallSet;
+        unordered_set<int> guardSet;
+        unordered_set<int> visited;
+
+        for (auto& w : walls)
+            wallSet.insert(w[0] * n + w[1]);
+
+        for (auto& g : guards)
+            guardSet.insert(g[0] * n + g[1]);
+
         int count = m * n - guards.size() - walls.size();
-        unordered_set<vector<int>, VectorHash> visited;
-        for (vector<int> guard : guards) {
+
+        for (auto& guard : guards) {
             int x = guard[0];
             int y = guard[1];
-            int index = x + 1;
-            while (index < m && !guardSet.contains({index, y}) && !wallSet.contains({index, y})) {
-                visited.insert({index, y});
-                index++;
+
+            // down
+            for (int i = x + 1; i < m; i++) {
+                int pos = i * n + y;
+
+                if (guardSet.contains(pos) || wallSet.contains(pos))
+                    break;
+
+                visited.insert(pos);
             }
 
-            index = y + 1;
-            while (index < n && !guardSet.contains({x, index}) && !wallSet.contains({x, index})) {
-                visited.insert({x, index});
-                index++;
+            // right
+            for (int j = y + 1; j < n; j++) {
+                int pos = x * n + j;
+
+                if (guardSet.contains(pos) || wallSet.contains(pos))
+                    break;
+
+                visited.insert(pos);
             }
 
-            index = x - 1;
-            while (index >= 0 && !guardSet.contains({index, y}) && !wallSet.contains({index, y})) {
-                visited.insert({index, y});
-                index--;
+            // up
+            for (int i = x - 1; i >= 0; i--) {
+                int pos = i * n + y;
+
+                if (guardSet.contains(pos) || wallSet.contains(pos))
+                    break;
+
+                visited.insert(pos);
             }
 
-            index = y - 1;
-            while (index >= 0 && !guardSet.contains({x, index}) && !wallSet.contains({x, index})) {
-                visited.insert({x, index});
-                index--;
+            // left
+            for (int j = y - 1; j >= 0; j--) {
+                int pos = x * n + j;
+
+                if (guardSet.contains(pos) || wallSet.contains(pos))
+                    break;
+
+                visited.insert(pos);
             }
         }
+
         return count - visited.size();
     }
 };
