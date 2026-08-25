95fd81c19a9e7dfb9797dc6ace42e2d67a11a246
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2026-08-25 16:06:42 -0400

    Add LeetCode solution: 2257. Count Unguarded Cells In The Grid

--- 2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid.cpp
@@ -3,63 +3,95 @@ public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards,
                        vector<vector<int>>& walls) {
 
-        unordered_set<int> wallSet;
-        unordered_set<int> guardSet;
-        unordered_set<int> visited;
-
-        for (auto& w : walls)
-            wallSet.insert(w[0] * n + w[1]);
+        // 0 = empty
+        // 1 = guard
+        // 2 = wall
+        // 3 = guarded
+        vector<vector<int>> grid(m, vector<int>(n, 0));
 
         for (auto& g : guards)
-            guardSet.insert(g[0] * n + g[1]);
+            grid[g[0]][g[1]] = 1;
 
-        int count = m * n - guards.size() - walls.size();
+        for (auto& w : walls)
+            grid[w[0]][w[1]] = 2;
 
-        for (auto& guard : guards) {
-            int x = guard[0];
-            int y = guard[1];
+        // Left -> Right
+        for (int i = 0; i < m; i++) {
+            bool seenGuard = false;
 
-            // down
-            for (int i = x + 1; i < m; i++) {
-                int pos = i * n + y;
-
-                if (guardSet.contains(pos) || wallSet.contains(pos))
-                    break;
-
-                visited.insert(pos);
-            }
-
-            // right
-            for (int j = y + 1; j < n; j++) {
-                int pos = x * n + j;
-
-                if (guardSet.contains(pos) || wallSet.contains(pos))
-                    break;
-
-                visited.insert(pos);
-            }
-
-            // up
-            for (int i = x - 1; i >= 0; i--) {
-                int pos = i * n + y;
-
-                if (guardSet.contains(pos) || wallSet.contains(pos))
-                    break;
-
-                visited.insert(pos);
-            }
-
-            // left
-            for (int j = y - 1; j >= 0; j--) {
-                int pos = x * n + j;
-
-                if (guardSet.contains(pos) || wallSet.contains(pos))
-                    break;
-
-                visited.insert(pos);
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    seenGuard = false;
+                }
+                else if (grid[i][j] == 1) {
+                    seenGuard = true;
+                }
+                else if (seenGuard) {
+                    grid[i][j] = 3;
+                }
             }
         }
 
-        return count - visited.size();
+        // Right -> Left
+        for (int i = 0; i < m; i++) {
+            bool seenGuard = false;
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 2) {
+                    seenGuard = false;
+                }
+                else if (grid[i][j] == 1) {
+                    seenGuard = true;
+                }
+                else if (seenGuard) {
+                    grid[i][j] = 3;
+                }
+            }
+        }
+
+        // Top -> Bottom
+        for (int j = 0; j < n; j++) {
+            bool seenGuard = false;
+
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == 2) {
+                    seenGuard = false;
+                }
+                else if (grid[i][j] == 1) {
+                    seenGuard = true;
+                }
+                else if (seenGuard) {
+                    grid[i][j] = 3;
+                }
+            }
+        }
+
+        // Bottom -> Top
+        for (int j = 0; j < n; j++) {
+            bool seenGuard = false;
+
+            for (int i = m - 1; i >= 0; i--) {
+                if (grid[i][j] == 2) {
+                    seenGuard = false;
+                }
+                else if (grid[i][j] == 1) {
+                    seenGuard = true;
+                }
+                else if (seenGuard) {
+                    grid[i][j] = 3;
+                }
+            }
+        }
+
+        int unguarded = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0)
+                    unguarded++;
+            }
+        }
+
+        return unguarded;
     }
 };
