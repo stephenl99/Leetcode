598f7240a6d2a8813ea941e5fa65af268da51c0e
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2026-08-31 09:15:25 -0400

    Add LeetCode solution: 2772. Apply Operations To Make All Array Elements Equal To Zero

---
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkArray(vector<int>& nums, int k) {
+        int curr = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] - curr < 0) {
+                return false;
+            } 
+            nums[i] -= curr;
+            curr += nums[i];
+            if (i >= k - 1) {
+                curr -= nums[i - k + 1];
+            }
+        }
+        return curr == 0;
+    }
+};
