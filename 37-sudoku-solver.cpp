30c39836f49bb405a5dd0b4be44ccb0e35af641e
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2026-09-02 13:48:31 -0400

    Add LeetCode solution: 37. Sudoku Solver

--- 37-sudoku-solver.cpp
+++ 37-sudoku-solver.cpp
@@ -1,78 +1,79 @@
 class Solution {
 public:
+    vector<vector<char>> board;
     int n = 3;
     int N = n * n;
-    int **rows;
-    int **cols;
-    int **boxes;
-    vector<vector<char>> board;
-    bool solved = false;
+    vector<vector<int>> rows;
+    vector<vector<int>> cols;
+    vector<vector<int>> boxes;
+    bool solved;
 
-    bool could_place(int d, int row, int col) {
+    bool can_insert(int i, int row, int col) {
         int idx = (row / n) * n + col / n;
-        return rows[row][d] + cols[col][d] + boxes[idx][d] == 0;
+        return rows[row][i] + cols[col][i] + boxes[idx][i] == 0;
     }
 
-    void place_number(int d, int row, int col) {
+    void insert_number(int i, int row, int col) {
         int idx = (row / n) * n + col / n;
-        rows[row][d]++;
-        cols[col][d]++;
-        boxes[idx][d]++;
-        board[row][col] = (char)(d + '0');
+        rows[row][i]++;
+        cols[col][i]++;
+        boxes[idx][i]++;
+        board[row][col] = (char) (i + '0');
     }
-    void remove_number(int d, int row, int col) {
+
+    void delete_number(int i, int row, int col) {
         int idx = (row / n) * n + col / n;
-        rows[row][d]--;
-        cols[col][d]--;
-        boxes[idx][d]--;
+        rows[row][i]--;
+        cols[col][i]--;
+        boxes[idx][i]--;
         board[row][col] = '.';
     }
-    void place_next_numbers(int row, int col) {
+
+    void next_numbers(int row, int col) {
         if (row == N - 1 && col == N - 1) {
             solved = true;
         } else {
             if (col == N - 1) {
-                backtrack(row + 1, 0);
+                recurse(row + 1, 0);
             } else {
-                backtrack(row, col + 1);
+                recurse(row, col + 1);
             }
         }
     }
-    void backtrack(int row, int col) {
+
+    void recurse(int row, int col) {
         if (board[row][col] == '.') {
             for (int i = 1; i < 10; i++) {
-                if (could_place(i, row, col)) {
-                    place_number(i, row, col);
-                    place_next_numbers(row, col);
+                if (can_insert(i, row, col)) {
+                    insert_number(i, row, col);
+                    next_numbers(row, col);
                     if (!solved) {
-                        remove_number(i, row, col);
+                        delete_number(i, row, col);
                     }
                 }
             }
         } else {
-            place_next_numbers(row, col);
+            next_numbers(row, col);
         }
     }
+
     void solveSudoku(vector<vector<char>>& board) {
         this->board = board;
-        rows = new int *[N];
-        cols = new int *[N];
-        boxes = new int *[N];
-        for (int i = 0; i < N; i++) {
-            rows[i] = new int[N + 1]();
-            cols[i] = new int[N + 1]();
-            boxes[i] = new int[N + 1]();
-        }
+        this->rows = vector<vector<int>>(N, vector<int>(N + 1, 0));
+        this->cols = vector<vector<int>>(N, vector<int>(N + 1, 0));
+        this->boxes = vector<vector<int>>(N, vector<int>(N + 1, 0));
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                char num = board[i][j];
-                if (num != '.') {
-                    int d = num - '0';
-                    place_number(d, i, j);
+                if (board[i][j] != '.') {
+                    int idx = (i / n) * n + j / n;
+                    int d = board[i][j] - '0';
+                    rows[i][d]++;
+                    cols[j][d]++;
+                    boxes[idx][d]++;
                 }
             }
         }
-        backtrack(0, 0);
+        recurse(0, 0);
         board = this->board;
     }
 };
