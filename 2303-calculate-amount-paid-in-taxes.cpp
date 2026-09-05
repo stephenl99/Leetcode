428b5cfce9fea95b3e3e0bb5fd15dd5a9042191c
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2026-09-05 14:08:52 -0400

    Add LeetCode solution: 2303. Calculate Amount Paid In Taxes

--- 2303-calculate-amount-paid-in-taxes.cpp
+++ 2303-calculate-amount-paid-in-taxes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    double calculateTax(vector<vector<int>>& brackets, int income) {
-        int remaining = income;
-        double cost = 0;
-        int prev = 0;
-        for (vector<int> bracket : brackets) {
-            int size = bracket[0] - prev;
-            cost += min(size, remaining) * .01 * bracket[1];
-            if (size >= remaining) {
-                return cost;
-            }
-            remaining -= size;
-            prev = bracket[0];
+    double calculateTax(vector<vector<int>>& brackets, int income) 
+    {   
+        if(income==0)
+           return 0.0;
+        double ans=0;
+        int prev=0; 
+        for(int i=0;i<brackets.size();i++)
+        {
+            int curr=min(brackets[i][0],income);
+            ans+=((curr-prev)*brackets[i][1]/100.0);
+            if(brackets[i][0]>=income)
+               break;
+            prev=brackets[i][0];
         }
-        return cost;
+        return ans; 
     }
 };
