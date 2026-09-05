407e14b3bd949a47de4d24403070254970b345bc
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2026-09-05 19:36:22 -0400

    Add LeetCode solution: 188. Best Time To Buy And Sell Stock Iv

--- 188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        int profit = 0;
-        if (n == 0) {
-            return 0;
-        }
-        if (2 * k >= n) {
-            for (int i = 1; i < n; i++) {
-                profit += max(0, prices[i] - prices[i - 1]);
-            }
-            return profit;
-        }
-        vector<vector<vector<int>>> dp = vector<vector<vector<int>>>(n, vector<vector<int>> (k + 1, vector<int> (2, 0)));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                dp[i][j][0] = INT_MIN / 2;
-                dp[i][j][1] = INT_MIN / 2;
+        vector<int> buy(k + 1, INT_MIN);
+        vector<int> sell(k + 1, 0);
+
+        for(auto p: prices){
+            for(int i=1;i<=k;i++){
+                buy[i] = max(buy[i], sell[i-1]-p);
+                sell[i] = max(sell[i], buy[i] + p);
             }
         }
-        dp[0][1][1] = -prices[0];
-        dp[0][0][0] = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
-                if (j > 0) {
-                     dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
-                }
-            }
-        }
-        int ans = 0;
-        for (int j = 0; j <= k; j++) {
-            ans = max(ans, dp[n - 1][j][0]);
-        }
-        return ans;
+
+        return sell[k];
     }
 };
