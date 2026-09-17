952b1795f4c28d05c966e85df85d9e1d24fcc207
Stephen Linder <146971681+stephenl99@users.noreply.github.com>
2026-09-17 17:48:08 -0400

    Add LeetCode solution: 1665. Minimum Initial Energy To Finish Tasks

--- 1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
-        sort(tasks.begin(), tasks.end(), [&](vector<int>& a, vector<int>& b) {
-            return a[1] - a[0] < b[1] - b[0];
+        sort(tasks.begin(), tasks.end(), [](vector<int>& a, vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
         });
-        int ans = 0;
-        for (auto task : tasks) {
-            ans = max(ans + task[0], task[1]);
+        long long e = 0;
+        long long s = 0;
+        for (auto& t : tasks) {
+            e = max(e, s + t[1]);
+            s += t[0];
         }
-        return ans;
+        return (int)e;
     }
 };
